5db5e6ea574267eeef9f66a7d252aadd48e0f0db
aymenimos <aarreeggwweell@gmail.com>
2022-12-28 16:43:37 +0300

    push pop top done

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
 
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        tostack stack=new tostack();
+        stack.CreatStack();
+
+        stack.push(29);
+        stack.push(14);
+        stack.push(56);
+        //stack.display();
+        // stack.top();
+      // System.out.println(stack.pop()); ;
+        System.out.println("null");
+        // stack.top();
+       stack.popuntilempty();
+
+    
     }
     
 }
--- tostack.java
+++ tostack.java
@@ -1,17 +1,74 @@
 public class tostack {
-    
-int [] arr= new int[10];
 
+    private int size = 5;
+    private int[] arr;
+    private int top;
+   private int maxtop;
 
+    public void CreatStack() {
+        top = -1;
+       maxtop = size - 1;
+        arr = new int[size];
+    }
 
+    public void push(int number) {
+        if (isfull()) {
+            System.out.println("the stack is full");
 
-private void pop() {
+        } else {
 
-    
+            top++;
+            arr[top] = number;
+
+        }
+    }
+
+    public double pop() {
+        if (isempty()) {
+            System.out.println("the stack is empty from pop method");
+        } else {
+
+            // double store= arr[top];
+            // top--;
+            // System.out.println("the top element is: " + store );
+
+        }
+        return arr[top--];
+    }
+
+    public boolean isfull() {
+
+        return top == maxtop - 1;
+    }
+
+    private boolean isempty() {
+        return top == -1;
+
+    }
+
+    public void top() {
+
+        if (isempty()) {
+            System.out.println("the stack is empty from top method");
+        } else {
+            System.out.println(arr[top] + " from top method");
+        }
+
+    }
+
+    public void popuntilempty() {
+        while (isempty() == false) {
+            System.out.println(arr[top]);
+            top--;
+        }
+
+    }
+
+    public void display() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
 }
-private void push(int number) {
-    
-}
-
-}
-
